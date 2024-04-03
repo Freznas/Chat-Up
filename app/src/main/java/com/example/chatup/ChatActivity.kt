bbaf3c020b1cfce22d0cff3b6d74b36d6fe3f56d
Joakim Bjärkstedt <joakim198904@live.se>
2024-04-03 14:04:05 +0200

    Created Activities for the different views of the app so far.

--- app/src/main/java/com/example/chatup/ChatActivity.kt
+++ app/src/main/java/com/example/chatup/ChatActivity.kt
@@ -1,11 +1,11 @@
 package com.example.chatup
-// Chat Up ! Chattapplication with Achievments, Build with MVC Structure.
+// Activity to Display the active Chat between two or more users.
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class ChatActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_chat)
     }
 }
--- app/src/main/java/com/example/chatup/LoginActivity.kt
+++ app/src/main/java/com/example/chatup/LoginActivity.kt
@@ -1,11 +1,11 @@
 package com.example.chatup
-// Chat Up ! Chattapplication with Achievments, Build with MVC Structure.
+// Activity for handling input of login information to log in the correct user.
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
