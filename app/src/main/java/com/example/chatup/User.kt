704e84c574adac5e7ee53b5921f30072397cc07c
maxhendess <nekohigan@gmail.com>
2024-04-04 15:24:03 +0200

    add User

---
+++ app/src/main/java/com/example/chatup/User.kt
@@ -0,0 +1,9 @@
+package com.example.chatup
+
+import java.net.URL
+
+class User (val id: String?,
+            val name: String?,
+            val password: String?,
+            val presentation: String?,
+            val profilePicture: URL?)
