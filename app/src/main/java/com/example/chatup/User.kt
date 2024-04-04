709351b44fd5b7001fcaba4c2a29e2297418d126
maxhendess <nekohigan@gmail.com>
2024-04-04 16:23:26 +0200

    add UserDao (missing updateUser)

--- app/src/main/java/com/example/chatup/User.kt
+++ app/src/main/java/com/example/chatup/User.kt
@@ -4,6 +4,14 @@ import java.net.URL
 
 class User (val id: String?,
             val name: String?,
-            val password: String?,
-            val presentation: String?,
-            val profilePicture: URL?)
+            val password: String?
+            ){
+    // When make User presentation and pic is optional
+    var presentation: String? ="placeholder"
+   var profilePicture: URL? = URL("https://www.youtube.com/")
+    override fun toString(): String {
+        return "User\nname: $name\npassword: $password\npresentation: $presentation\nprofilePicture: $profilePicture"
+    }
+
+
+}
