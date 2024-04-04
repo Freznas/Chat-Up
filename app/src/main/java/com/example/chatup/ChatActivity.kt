9e0c77503955225dd0a8ef99c4ca0acf7012c5cd
Dennis <nilssondennis97@gmail.com>
2024-04-04 13:12:36 +0200

    Implemented viewbinding for every activity

--- app/src/main/java/com/example/chatup/ChatActivity.kt
+++ app/src/main/java/com/example/chatup/ChatActivity.kt
@@ -2,10 +2,13 @@ package com.example.chatup
 // Activity to Display the active Chat between two or more users.
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.chatup.databinding.ActivityChatBinding
 
 class ChatActivity : AppCompatActivity() {
+    lateinit var binding: ActivityChatBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_chat)
+        binding = ActivityChatBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
--- app/src/main/java/com/example/chatup/SearchUserActivity.kt
+++ app/src/main/java/com/example/chatup/SearchUserActivity.kt
@@ -2,10 +2,16 @@ package com.example.chatup
 // Activity to handle finding users and adding friends/Block Users.
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.chatup.databinding.ActivitySearchUserBinding
 
 class SearchUserActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivitySearchUserBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_search_user)
+
+        binding = ActivitySearchUserBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
