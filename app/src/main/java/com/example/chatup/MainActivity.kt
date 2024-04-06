44711b8f5d0c00f5dd5d01cef78c24a5494f131c
maxhendess <nekohigan@gmail.com>
2024-04-06 21:15:18 +0200

    add Login functinallity

--- app/src/main/java/com/example/chatup/MainActivity.kt
+++ app/src/main/java/com/example/chatup/MainActivity.kt
@@ -4,10 +4,13 @@ package com.example.chatup
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.chatup.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    var userDao = UserDao()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -17,5 +20,24 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
         }
+            binding.btnSignin.setOnClickListener {
+                login()
+            }
+
+    }
+
+     fun login() {
+        val username = binding.username.text.toString()
+        val password = binding.password.text.toString()
+         userDao.checkUserCredentials(username, password){ validCredentials ->
+             if (validCredentials) {
+                 println("User exist")
+                 val intent = Intent(this, ChatActivity::class.java)
+                 startActivity(intent)
+             } else {
+                 // User does not exist, perform further actions
+                 println("Not valid credantials")
+             }
+         }
     }
 }
