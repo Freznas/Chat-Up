9315262f46e6dda72957b533e5bad6129153e2dd
Joakim Bjärkstedt <joakim198904@live.se>
2024-04-07 18:37:39 +0200

    finished prototype for search user functionality.

--- app/src/main/java/com/example/chatup/SearchUserActivity.kt
+++ app/src/main/java/com/example/chatup/SearchUserActivity.kt
@@ -2,16 +2,62 @@ package com.example.chatup
 // Activity to handle finding users and adding friends/Block Users.
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.SearchView
+import android.widget.Toast
 import com.example.chatup.databinding.ActivitySearchUserBinding
+
+
+
 class SearchUserActivity : AppCompatActivity() {
     lateinit var binding: ActivitySearchUserBinding
-
+    lateinit var searchView: SearchView
+    lateinit var listView: ListView
+    lateinit var userDao: UserDao
+    lateinit var adapter: ArrayAdapter<String>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivitySearchUserBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        userDao = UserDao()
+
+        searchView = findViewById(R.id.sv_search_user)
+        listView = findViewById(R.id.lv_search_user)
+
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1)
+        listView.adapter = adapter
+
+        listView.setOnItemClickListener { _, _, position, _ ->
+            val selectedUsername = adapter.getItem(position)
+            Toast.makeText(this, "Selected user $selectedUsername", Toast.LENGTH_LONG).show()
+
+        }
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                if (newText!!.isNotEmpty()) {
+                    performSearch(newText)
+                } else {
+                    adapter.clear()
+                }
+                return true
+
+            }
+        })
+    }
+         fun performSearch(query: String) {
+            userDao.searchUsers(query) { users ->
+                val userNames = users.map { it.name }
+                adapter.clear()
+                adapter.addAll(userNames)
+            }
+        }
 
     }
-}
+
--- app/src/main/java/com/example/chatup/UserDao.kt
+++ app/src/main/java/com/example/chatup/UserDao.kt
@@ -1,7 +1,6 @@
 package com.example.chatup
 
 import android.util.Log
-import com.google.firebase.firestore.FieldPath
 import com.google.firebase.firestore.FirebaseFirestore
 import java.net.URL
 
@@ -84,5 +83,33 @@ class UserDao {
                 Log.i("SUCCSESS", " FETCHED USERS FROM FIRESTORE")
                 activity.showUsers(users)
             }.addOnFailureListener { log -> Log.e("ERROR", "Failed to fetch USERS from firestore") }
+    }
+    fun searchUsers(query:String,callback:(List<User>)->Unit){
+        val users = mutableListOf<User>()
+        FirebaseFirestore.getInstance()
+            .collection("users")
+            .whereEqualTo(KEY_NAME,query)
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result)
+                {
+                    val id = document.getString(KEY_ID)?:""
+                    val name =document.getString(KEY_NAME)?:""
+                    val password = document.getString(KEY_PASSWORD)?:""
+                    val email = document.getString(KEY_EMAIL)?:""
+                    val presentation = document.getString(KEY_PRESENTATION)?:""
+                    val profilePicture = document.getString(KEY_PROFILEPICTURE)?:""
+
+                    val user=User(id,name,password,email)
+                    user.presentation=presentation
+                    user.profilePicture=URL(profilePicture)
+                    users.add(user)
+
+                }
+                callback(users)
+            }.addOnFailureListener {
+                Log.e("ERROR","Failed to search users")
+                callback(emptyList())
+            }
     }
 }
