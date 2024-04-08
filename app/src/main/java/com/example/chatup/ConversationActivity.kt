ebce957f6e185339e9df33496908b2b6a70d4668
Dennis <nilssondennis97@gmail.com>
2024-04-08 11:40:40 +0200

    Conversations view, with navigation

--- app/src/main/java/com/example/chatup/ConversationActivity.kt
+++ app/src/main/java/com/example/chatup/ConversationActivity.kt
@@ -1,5 +1,6 @@
 package com.example.chatup
 //Activity to display the different conversations active by the logged in user
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.chatup.databinding.ActivityConversationBinding
@@ -10,5 +11,15 @@ class ConversationActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityConversationBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnMyProfile.setOnClickListener {
+            val intent = Intent(this, ProfileActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnSearchUser.setOnClickListener {
+            val intent = Intent(this, SearchUserActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
